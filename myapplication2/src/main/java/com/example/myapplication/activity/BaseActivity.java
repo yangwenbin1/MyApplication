1f17f377f56b452f6e1d1debef52c9be172a31bc
Your Name <email@example.com>
2017-09-27 20:25:53 +0800

    Initial commit

---
+++ myapplication2/src/main/java/com/example/myapplication/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dellll on 2017.09.27.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ myapplication2/src/main/java/com/example/myapplication/activity/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class SecondActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
